873d001538f31066795e9a95bec3c288d9768634
Manish1678.sos <manishbhowmik1234@gmail.com>
2026-03-21 23:18:18 +0530

    First Commit

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
+const tempField = document.querySelector(".temperature p");
+const locationField = document.querySelector(".place p");
+const timeField = document.querySelector(".time p");
+const conditionField = document.querySelector(".condition p");
 
+const form = document.querySelector("#Weather-form");
+const searchField = document.querySelector("#city");
 
+let targetLocation = "Kolkata";
 
-const fetchResults=async()=>
-{
-    let url="http://api.weatherapi.com/v1/current.json?key=c417664c6d58487380c71034222609&q=Mumbai&aqi=no";
-    const res= await fetch(url);
-    const data=await res.json(); 
-    
+// form submit
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  targetLocation = searchField.value;
+  fetchWeather(targetLocation);
+});
+
+// fetch weather
+async function fetchWeather(city) {
+  try {
+  let url = `https://api.weatherapi.com/v1/current.json?key=YOUR_KEY&q=Kolkata&aqi=no`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+
     console.log(data);
-    let locationName=data.location.name
-    let time=data.location.locationtime
-    let temp=data.current.temp_c;
-    let condition=data.current.condition.text;
+
+    if (data.error) {
+      alert("City not found!");
+      return;
+    }
+
+    updateUI(data);
+
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+// update UI
+function updateUI(data) {
+  tempField.innerText = data.current.temp_c + "°C";
+  locationField.innerText = data.location.name;
+  timeField.innerText = data.location.localtime;
+  conditionField.innerText = data.current.condition.text;
+}
+
+// default load
+fetchWeather(targetLocation);
